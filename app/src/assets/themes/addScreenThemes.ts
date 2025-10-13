c084f4442e3467b11699798ea62a922cf67778ea
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-10-13 23:52:35 +0530

    feat: styled record button in /add screen

---
+++ app/src/assets/themes/addScreenThemes.ts
@@ -0,0 +1,7 @@
+export const COLOR_TRANSITION = [
+    "#38bdf8", // sky-400
+    "#22d3ee", // cyan-400
+    "#a78bfa", // purple-400
+    "#f472b6", // pink-400
+    "#facc15", // yellow-400
+];
--- app/src/tabs/AddScreen.tsx
+++ app/src/tabs/AddScreen.tsx
@@ -1,7 +1,50 @@
-import { Text } from 'react-native'
+import { Text, View, Pressable } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
-export default function AddScreen() {
+import Wave from '../components/Wave';
+import { COLOR_TRANSITION } from '../assets/themes/addScreenThemes';
+
+export default function ViewTodos() {
+    const [isRecording, setIsRecording] = useState(false);
+    const [colorIndex, setColorIndex] = useState(0);
+
+    useEffect(() => {
+        if (isRecording) {
+            const interval = setInterval(() => {
+                setColorIndex((prev) => (prev + 1) % COLOR_TRANSITION.length);
+                }, 1000);
+                return () => clearInterval(interval);
+        }
+
+    }, [isRecording]);
+
     return (
-        <Text>Add Screen</Text>
-    )
+        <View className="flex-1 bg-slate-950 justify-center items-center">
+            <Pressable
+                onPress={() => setIsRecording(!isRecording)}
+                className="border-4 border-gray-200 w-64 aspect-square justify-center items-center rounded-full transition-all duration-300"
+                style={{
+                    borderColor: COLOR_TRANSITION[colorIndex],
+                }}
+            >
+
+                {isRecording ? (
+                    <View className="grid grid-rows-[3fr_1fr]">
+                        <View className="flex justify-center items-center">
+                            <Wave />
+                        </View>
+                        <Text className="text-white font-bold text-2xl text-center">
+                            Listening...
+                        </Text>
+                    </View>
+                ) : (
+                    <Text className="text-white font-bold text-2xl">
+                        Start
+                    </Text>
+                )}
+
+            </Pressable>
+
+        </View>
+    );
 }
