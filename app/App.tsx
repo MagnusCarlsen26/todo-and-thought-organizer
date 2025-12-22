60f0cc7802efac88ce4db297408f121c20888239
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2025-12-22 16:36:45 +0530

    commit to delete

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,17 @@ import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { DarkTheme, NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ViewTodos from './src/tabs/ViewTodos';
-import AddScreen from './src/tabs/AddScreen';
-import ViewNotes from './src/tabs/ViewNotes';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import requestNotificationPermission from './src/utils/permissions/notificationPermission';
 import * as Notifications from 'expo-notifications';
+import { AddIcon, TodosIcon, NotesIcon } from './src/assets/svgs/navSvgs';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+
+import AddScreen from './src/tabs/AddScreen';
+import ViewTodos from './src/tabs/ViewTodos';
+import ViewNotes from './src/tabs/ViewNotes';
+import SideBar from './src/components/sideBar/sideBar';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -18,11 +24,8 @@ Notifications.setNotificationHandler({
   }),
 });
 
-import { AddIcon, TodosIcon, NotesIcon } from './src/assets/svgs/navSvgs';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
 
@@ -34,47 +37,54 @@ export default function App() {
     <GestureHandlerRootView className="flex-1 bg-slate-950">
       <NavigationContainer theme={DarkTheme}>
         <StatusBar style="light" backgroundColor="#0b0f16" />
-        <Tab.Navigator 
-          screenOptions={screenOptions}
-          initialRouteName="Add"
-        >
-        <Tab.Screen
-          name="Todos"
-          component={ViewTodos}
-          options={{
-            tabBarIcon: ({ color, size }) => <TodosIcon color={color} size={size} />
-          }} 
-        />
-        <Tab.Screen
-          name="Add"
-          component={AddScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => <AddIcon color={color} size={size} />
-          }} 
-        />
-        <Tab.Screen
-          name="Notes"
-          component={ViewNotes}
-          options={{
-            tabBarIcon: ({ color, size }) => <NotesIcon color={color} size={size} />
-          }} 
-        />
-        
-        </Tab.Navigator>
+        <Drawer.Navigator drawerContent={props => <SideBar />}>
+          <Drawer.Screen name=" " component={AppTabs} />
+        </Drawer.Navigator>
       </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
 
-const screenOptions = {
-  headerShown: false,
-  tabBarActiveTintColor: '#93c5fd',
-  tabBarInactiveTintColor: '#6b7280',
-  tabBarStyle: {
-    backgroundColor: '#0b0f16',
-    borderTopColor: '#111827'
-  },
-  tabBarLabelStyle: {
-    fontSize: 12
-  }
+const AppTabs = () => {
+  return (
+    <Tab.Navigator 
+      screenOptions={{
+        
+      }}
+      initialRouteName="Add"
+    >
+      <Tab.Screen
+        name="Todos"
+        component={ViewTodos}
+        options={{
+          tabBarIcon: ({ color, size }) => <TodosIcon color={color} size={size} />
+        }} 
+      />
+      <Tab.Screen
+        name="Add"
+        component={AddScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <AddIcon color={color} size={size} />
+        }} 
+      />
+      <Tab.Screen
+        name="Notes"
+        component={ViewNotes}
+        options={{
+          tabBarIcon: ({ color, size }) => <NotesIcon color={color} size={size} />
+        }} 
+      />
+    </Tab.Navigator>
+  );
 }
+
+const screenOptions = {headerShown: false,
+        tabBarActiveTintColor: '#93c5fd',
+        tabBarInactiveTintColor: '#6b7280',
+        tabBarStyle: {
+          backgroundColor: '#0b0f16',
+          borderTopColor: '#111827'
+        },
+        tabBarLabelStyle: {
+          fontSize: 12
+        }}
---
+++ app/src/components/sideBar/sideBar.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+
+export default function SideBar() {
+    console.log("GII")
+    return (
+        <Text>
+            Drawer
+        </Text>
+    )
+}
